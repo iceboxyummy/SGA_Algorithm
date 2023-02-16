89608fb6286699937362ab83f720cea69b64d032
kgb611@naver.com <kgb611@naver.com>
2023-02-16 15:48:51 +0900

    0216_Algorithm 7일차

--- Linked_list/02_Double_Linked_List.h
+++ Linked_list/02_Double_Linked_List.h
@@ -9,79 +9,174 @@ template<typename T>
 class DoubleLinkedList
 {
 public:
-    struct Node
-    {
-        Node* prev = nullptr;
-        Node* next = nullptr;
-        T data;
-    };
+	struct Node
+	{
+		Node* prev = nullptr;
+		Node* next = nullptr;
+		T data;
+	};
 
-    static Node* Create(T data)
-    {
-        Node* node = new Node();
-        node->data = data;
-        return node;
-    }
+	static Node* Create(T data)
+	{
+		Node* node = new Node();
+		node->data = data;
+		return node;
+	}
 
-    static void Destroy(Node** node)
-    {
-        delete* node;
-        *node = nullptr;
-    }
+	static void Destroy(Node** node)
+	{
+		delete* node;
+		*node = nullptr;
+	}
 
 public:
 
-    ~DoubleLinkedList()
-    {
-        Clear();
-    }
+	~DoubleLinkedList()
+	{
+		Clear();
+	}
 
-    void PushBack(T data)
-    {
+	void PushBack(T data)
+	{
+		Node* node = Create(data);
 
-    }
+		if (head == nullptr)
+		{
+			head = node;
+			tail = node;
+		}
 
-    void PushFront(T data)
-    {
+		else
+		{
+			tail->next = node;
+			node->prev = tail;
+			tail = node;
+		}
+		size++;
+	}
 
-    }
+	void PushFront(T data)
+	{
+		Node* node = Create(data);
 
-    void PopBack()
-    {
+		if (head == nullptr)
+		{
+			head = node;
+			tail = node;
+		}
 
-    }
+		else
+		{
+			head->prev = node;
+			node->next = head;
+			head = node;
+		}
+		size++;
+	}
 
-    void PopFront()
-    {
+	void PopBack()
+	{
+		if (size <= 0) return;
 
-    }
+		if (size == 1)
+		{
+			Destroy(&head);
+			tail = nullptr;
+		}
 
-    void Insert(Node* target, T data)
-    {
+		else
+		{
+			Node* temp = tail->prev;
+			Destroy(&tail);
+			tail = temp;
+			tail->next = nullptr;
+		}
+		size--;
+	}
 
-    }
+	void PopFront()
+	{
+		if (size <= 0) return;
 
+		if (size == 1)
+		{
+			Destroy(&head);
+			tail = nullptr;
+		}
 
-    Node* operator[](int index)
-    {
+		else
+		{
+			Node* temp = head->next;
+			Destroy(&head);
+			head = temp;
+			head->prev = nullptr;
+		}
+		size--;
+	}
 
-    }
+	void Insert(Node* target, T data)
+	{
+		Node* temp = head;
 
-    int Size() { return size; }
+		while (temp != nullptr)
+		{
+			if (temp == target)break;
+			temp = temp->next;
+		}
+		if (temp == nullptr)return;
 
-    void Clear()
-    {
+		if (target = head)
+			PushFront(data);
 
-    }
+		else
+		{
+			Node* new_node = Create(data);
 
-    void PrintList()
-    {
+			new_node->next = target;
+			new_node->prev = target->prev;
 
-    }
+			new_node->prev->next = new_node;
+			new_node->next->prev = new_node;
+
+			size++;
+		}
+	}
+
+	Node* operator[](int index)
+	{
+		if (index >= size)
+		{
+			cout << "index error" << endl;
+			return nullptr;
+		}
+
+		Node* node = head;
+
+		while (node != nullptr && index-- > 0)
+			node = node->next;
+
+		return node;
+	}
+
+	int Size() { return size; }
+
+	void Clear()
+	{
+		for (int i = size; i > 0; i--)
+			PopBack();
+	}
+
+	void PrintList()
+	{
+		for (int i = 0; i < size; i++)
+			cout << operator[](i)->data << " ";
+
+		cout << endl;
+	}
 
 protected:
-    Node* head = nullptr;
-    Node* tail = nullptr;
+	Node* head = nullptr;
+	Node* tail = nullptr;
 
-    int size = 0;
+	int size = 0;
 };
